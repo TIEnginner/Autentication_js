da0aad0f21aba341e5921fabc629d515132afb5a
Davi Luiz Teixeira Caboclo <daviltcaboclo@gmail.com>
2024-11-05 17:26:57 -0300

    O javascript ainda não está completo.

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyC6QnpxBD2B-VlOayuWlQzI3WzyWY8lCSk",
+  authDomain: "autentication-js.firebaseapp.com",
+  projectId: "autentication-js",
+  storageBucket: "autentication-js.firebasestorage.app",
+  messagingSenderId: "1076773848924",
+  appId: "1:1076773848924:web:28c2fa58f4804d28842399",
+  measurementId: "G-7F4KZCVX6K"
+};
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+function entry() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      alert("Seus dados foram cadastrados com sucesso");
+      document.getElementById("email").value = '';
+      document.getElementById("password").value = '';
+    })
+    .catch((error) => {
+      alert("Falha ao cadastrar: " + error.message);
+    });
+}
